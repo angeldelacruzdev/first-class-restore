393c473ec190b4473bb6c9ced54378f63b12d644
Ángel De La Cruz <8610043+angeldelacruzdev@users.noreply.github.com>
2023-09-09 10:17:17 -0400

    add: Meta tags

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
-import { HomeService } from './home.service';
+
 
 @Component({
   selector: 'app-home',
@@ -11,12 +11,24 @@ export class HomeComponent implements OnInit {
 
   public fruitList: any = [];
 
-  constructor(private readonly homeService: HomeService, private meta: Meta, private tile: Title) { }
+  constructor(private meta: Meta, private title: Title) { }
 
   ngOnInit(): void {
     this.meta.addTag({ name: "title", content: "First Class Restore" });
-    this.tile.setTitle("First Class Restore")
-    this.fruitList = this.homeService.findAll()
+    this.title.setTitle("First Class Restore")
+
+    // Establecer meta etiquetas
+    this.meta.updateTag({ name: 'description', content: ' This company is dedicated to delivering top-notch services, as reflected in our name. We were founded by Kilian Rodriguez and John Tavarez.' });
+
+    // Establecer Open Graph / Facebook meta etiquetas
+    this.meta.updateTag({ property: 'og:type', content: 'website' });
+    this.meta.updateTag({ property: 'og:url', content: 'https://www.firstclassrestore.com/' });
+    // ... otros tags
+
+    // Establecer Twitter meta etiquetas
+    this.meta.updateTag({ property: 'twitter:card', content: '../../assets/summary_large_image.png' });
+    // ... otros tags
+
   }
 }
 
