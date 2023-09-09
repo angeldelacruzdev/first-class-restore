7f60fe75d5f37e81362202e11fbfd95e8cba3473
Ángel De La Cruz <8610043+angeldelacruzdev@users.noreply.github.com>
2023-09-09 11:53:09 -0400

    add: new tags

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,6 +17,7 @@ export class HomeComponent implements OnInit {
     this.meta.addTag({ name: "title", content: "First Class Restore" });
     this.meta.addTag({ name: "robots", content: "follow" });
     this.meta.addTag({ name: "author", content: "John F Tavarez" });
+    this.meta.addTag({ name: "keywords", content: "  Water Damage,  Fire Restoration,  Industrial Cleanup, Odor Control,  Mold Remediation,   Bio-Recovery" });
     this.title.setTitle("First Class Restore")
 
     // Establecer meta etiquetas
@@ -26,11 +27,11 @@ export class HomeComponent implements OnInit {
     this.meta.updateTag({ property: 'og:type', content: 'website' });
     this.meta.updateTag({ property: 'og:url', content: 'https://www.firstclassrestore.com/' });
     this.meta.updateTag({ property: 'og:description', content: 'This company is dedicated to delivering top-notch services, as reflected in our name. We were founded by Kilian Rodriguez and John Tavarez.' });
-    this.meta.updateTag({ property: 'og:image', content: 'assets/summary_large_image.png' });
+    this.meta.updateTag({ property: 'og:image', content: '/assets/summary_large_image.png' });
     // ... otros tags
 
     // Establecer Twitter meta etiquetas
-    this.meta.updateTag({ property: 'twitter:card', content: 'assets/summary_large_image.png' });
+    this.meta.updateTag({ property: 'twitter:card', content: '/assets/summary_large_image.png' });
     this.meta.updateTag({ property: 'twitter:title', content: 'First Class Restore' });
     this.meta.updateTag({ property: 'twitter:url', content: 'https://www.firstclassrestore.com/' });
     this.meta.updateTag({ property: 'twitter:description', content: 'This company is dedicated to delivering top-notch services, as reflected in our name. We were founded by Kilian Rodriguez and John Tavarez.' });
